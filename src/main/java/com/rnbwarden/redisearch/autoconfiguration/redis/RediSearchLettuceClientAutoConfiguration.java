47f34db5cd401c0affee03cfc8ff65ca4099f4c1
jason.ward <jason.ward@bluestembrands.com>
2019-05-14 13:33:11 -0500

    working on Lettuce serialization issues

--- src/main/java/com/rnbwarden/redisearch/autoconfiguration/redis/RediSearchLettuceClientAutoConfiguration.java
+++ src/main/java/com/rnbwarden/redisearch/autoconfiguration/redis/RediSearchLettuceClientAutoConfiguration.java
@@ -4,13 +4,17 @@ import com.redislabs.lettusearch.StatefulRediSearchConnection;
 import com.redislabs.springredisearch.RediSearchConfiguration;
 import com.rnbwarden.redisearch.redis.client.lettuce.LettuceRediSearchClient;
 import io.lettuce.core.RedisClient;
+import io.lettuce.core.codec.ByteArrayCodec;
 import io.lettuce.core.codec.CompressionCodec;
 import io.lettuce.core.codec.RedisCodec;
 import io.lettuce.core.codec.StringCodec;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
+import java.nio.ByteBuffer;
 
 @Configuration("RediSearchLettuceClientAutoConfiguration")
 @ConditionalOnClass({RedisClient.class, com.redislabs.lettusearch.RediSearchClient.class})
@@ -24,9 +28,43 @@ public class RediSearchLettuceClientAutoConfiguration extends AbstractRediSearch
     @SuppressWarnings("unchecked")
     void createRediSearchBeans(Class<?> clazz) {
 
-        RedisCodec redisCodec = CompressionCodec.valueCompressor(StringCodec.UTF8, CompressionCodec.CompressionType.GZIP);
-        StatefulRediSearchConnection<byte[], byte[]> statefulRediSearchConnection = client.connect(redisCodec);
+//        RedisCodec<Object, Object> redisCodec = CompressionCodec.valueCompressor(StringCodec.UTF8, CompressionCodec.CompressionType.GZIP);
+        RedisCodec redisCodec = compressingRedisCodec();
+        StatefulRediSearchConnection<Object, Object> statefulRediSearchConnection = client.connect(redisCodec);
 
         beanFactory.registerSingleton(getRedisearchBeanName(clazz), new LettuceRediSearchClient(statefulRediSearchConnection, createRedisSerializer(clazz), defaultMaxResults));
+    }
+
+    private RedisCodec<String, byte[]> compressingRedisCodec() {
+
+        return new RedisCodec<String, byte[]>() {
+
+            private RedisCodec<String, String> keyCodec = StringCodec.UTF8;
+            private RedisCodec<byte[], byte[]> valueCodec = ByteArrayCodec.INSTANCE;
+
+            @Override
+            public String decodeKey(ByteBuffer bytes) {
+
+                return keyCodec.decodeKey(bytes);
+            }
+
+            @Override
+            public byte[] decodeValue(ByteBuffer bytes) {
+
+                return valueCodec.decodeValue(bytes);
+            }
+
+            @Override
+            public ByteBuffer encodeKey(String key) {
+
+                return keyCodec.encodeKey(key);
+            }
+
+            @Override
+            public ByteBuffer encodeValue(byte[] value) {
+
+                return valueCodec.encodeValue(value);
+            }
+        };
     }
 }
