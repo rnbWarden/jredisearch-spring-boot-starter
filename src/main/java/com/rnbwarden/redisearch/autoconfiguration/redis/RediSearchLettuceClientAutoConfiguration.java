5668b72c4a3da26fe77511b5ae782d5301bb95a2
jason.ward <jason.ward@bluestembrands.com>
2019-05-14 11:29:43 -0500

    working on Lettuce serialization issues

--- src/main/java/com/rnbwarden/redisearch/autoconfiguration/redis/RediSearchLettuceClientAutoConfiguration.java
+++ src/main/java/com/rnbwarden/redisearch/autoconfiguration/redis/RediSearchLettuceClientAutoConfiguration.java
@@ -4,6 +4,7 @@ import com.redislabs.lettusearch.StatefulRediSearchConnection;
 import com.redislabs.springredisearch.RediSearchConfiguration;
 import com.rnbwarden.redisearch.redis.client.lettuce.LettuceRediSearchClient;
 import io.lettuce.core.RedisClient;
+import io.lettuce.core.codec.ByteArrayCodec;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
@@ -15,11 +16,15 @@ import org.springframework.context.annotation.Import;
 public class RediSearchLettuceClientAutoConfiguration extends AbstractRediSearchClientAutoConfiguration {
 
     @Autowired
-    private StatefulRediSearchConnection<String, String> statefulRediSearchConnection;
+    private com.redislabs.lettusearch.RediSearchClient client;
+//    private StatefulRediSearchConnection<String, String> statefulRediSearchConnection;
 
     @Override
     @SuppressWarnings("unchecked")
     void createRediSearchBeans(Class<?> clazz) {
+
+        ByteArrayCodec byteArrayCodec = ByteArrayCodec.INSTANCE;
+        StatefulRediSearchConnection<byte[], byte[]> statefulRediSearchConnection = client.connect(byteArrayCodec);
 
         beanFactory.registerSingleton(getRedisearchBeanName(clazz), new LettuceRediSearchClient(statefulRediSearchConnection, createRedisSerializer(clazz), defaultMaxResults));
     }
