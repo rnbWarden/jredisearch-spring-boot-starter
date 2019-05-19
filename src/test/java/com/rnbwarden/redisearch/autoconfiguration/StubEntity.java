5d7c64f056a9a75b3a73f65f8113663241a2ba38
jason.ward <jason.ward@bluestembrands.com>
2019-05-19 12:28:16 -0500

    refactoring code clean-up

--- src/test/java/com/rnbwarden/redisearch/autoconfiguration/StubEntity.java
+++ src/test/java/com/rnbwarden/redisearch/autoconfiguration/StubEntity.java
@@ -1,4 +1,4 @@
-package com.rnbwarden.redisearch.redis;
+package com.rnbwarden.redisearch.autoconfiguration;
 
 import com.rnbwarden.redisearch.redis.entity.RediSearchEntity;
 import com.rnbwarden.redisearch.redis.entity.RedisSearchableEntity;
