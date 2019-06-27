044400c977d5a81e2693214b03ecd61e6717bd9a
jason.ward <jason.ward@bluestembrands.com>
2019-06-27 14:17:20 -0500

    re-ignoring unused test

--- src/test/java/com/rnbwarden/redisearch/LettuceSkuPagingTest.java
+++ src/test/java/com/rnbwarden/redisearch/LettuceSkuPagingTest.java
@@ -13,6 +13,7 @@ import io.lettuce.core.RedisURI;
 import io.lettuce.core.codec.RedisCodec;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.serializer.RedisSerializer;
@@ -26,7 +27,7 @@ import static java.lang.String.format;
 
 //import org.slf4j.Logger;
 
-//@Ignore // un-ignore to test with local redis w/ Search module
+@Ignore // un-ignore to test with local redis w/ Search module
 public class LettuceSkuPagingTest {
 
 //    private static final Logger LOGGER = LoggerFactory.getLogger(LettuceSkuTest.class);
