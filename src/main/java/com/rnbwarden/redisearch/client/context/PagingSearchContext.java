3056aa4a3c27e37a6a0dd5d6270526c1b4350ab2
jason.ward <jason.ward@bluestembrands.com>
2019-08-03 18:57:09 -0500

    lombok clean-up

--- src/main/java/com/rnbwarden/redisearch/client/context/PagingSearchContext.java
+++ src/main/java/com/rnbwarden/redisearch/client/context/PagingSearchContext.java
@@ -1,8 +1,10 @@
 package com.rnbwarden.redisearch.client.context;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class PagingSearchContext extends SearchContext {
 
     private boolean useClientSidePaging = false;
